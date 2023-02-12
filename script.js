4cf3199f8b472677abfd0fc9601252fa161a38f6
HimanshuGorane <117897707+HimanshuGorane@users.noreply.github.com>
2023-02-12 15:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 //your code here
-document.getElementById("btn").addEventListener('click',function() {
-	var num = Math.floor(Math.random()*41)+20; //By using the formula(max-min+1)+min
-	document.getElementById("num").innerHTML=num;
-});
+const btn = document.querySelector("#btn");
+  const num = document.querySelector("#num");
+  const guessBtn = document.querySelector("#guessBtn");
+  const guess = document.querySelector("#guess");
+  const result = document.querySelector("#result");
+  let randomNumber;
+
+  btn.addEventListener("click", function() {
+    randomNumber = Math.floor(Math.random() * 41) - 20;
+    num.textContent = "The number to guess is: " + randomNumber;
+  });
+
+  guessBtn.addEventListener("click", function() {
+    let userGuess = parseInt(guess.value);
+    let difference = Math.abs(userGuess - randomNumber);
+    if (difference <= 5) {
+      result.textContent = "Hot!";
+    } else {
+      result.textContent = "Cold!";
+    }
+  });
