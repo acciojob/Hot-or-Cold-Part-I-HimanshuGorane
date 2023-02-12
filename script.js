616a2f02f74d58b4c8d4935d25b85d0bd17b5bc5
HimanshuGorane <117897707+HimanshuGorane@users.noreply.github.com>
2023-02-12 09:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //your code here
-
+document.getElementById("btn").addEventListener('click',function() {
+	var num = Math.floor(Math.random()*41)+20; //By using the formula(max-min+1)+min
+	document.getElementById("num").innerHTML=num;
+});
